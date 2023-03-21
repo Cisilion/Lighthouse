e39d5a074276b063c1d5d6ac1f5d8ff7abf16cb6
marshalexander99 <55112990+marshalexander99@users.noreply.github.com>
2023-03-21 15:40:57 +0000

    updated PowerShell deployment script

--- deployonline.ps1
+++ deployonline.ps1
@@ -1,3 +1,5 @@
+Connect-AzAccount
+
 $read = New-Object System.Management.Automation.Host.ChoiceDescription '&Read'
 $manage = New-Object System.Management.Automation.Host.ChoiceDescription '&Manage'
 $options = [System.Management.Automation.Host.ChoiceDescription[]] ($read, $manage)
@@ -15,24 +17,30 @@ $submessage = 'Please select whether to grant for all subscriptions or just a si
 $subresult = $host.ui.PromptForChoice($subtitle, $submessage, $suboptions, 1)
 
 if ($subresult -eq 0) {
-    $subs = Get-AzSubscription | Out-GridView -Title "Select Subscription" | Select-AzSubscription
+    Get-AzSubscription | Out-GridView -Title "Select Subscription" -PassThru | Select-AzSubscription
+    if ($result -eq 0 ){
+            New-AzDeployment -location uksouth -TemplateUri "https://raw.githubusercontent.com/Cisilion-Ltd/Lighthouse/main/CisilionReader.json"
+            }
+        
+        if ($result -eq 1){
+                New-AzDeployment -location uksouth -TemplateUri "https://raw.githubusercontent.com/Cisilion-Ltd/Lighthouse/main/CisilionManager.json"
+            }
 }
 
 if ($subresult -eq 1) {
 $subs = Get-AzSubscription
-}
-
 if ($result -eq 0 ){
-foreach ($sub in $subs) {
-    Select-AzSubscription -SubscriptionId $sub.Id
-    New-AzDeployment -location uksouth -TemplateUri "https://raw.githubusercontent.com/Cisilion-Ltd/Lighthouse/main/CisilionReader.json"
-    }
-}
-
-if ($result -eq 1){
-    foreach ($sub in $subs) {    
+    foreach ($sub in $subs) {
         Select-AzSubscription -SubscriptionId $sub.Id
-        New-AzDeployment -location uksouth -TemplateUri "https://raw.githubusercontent.com/Cisilion-Ltd/Lighthouse/main/CisilionManager.json"
+        New-AzDeployment -location uksouth -TemplateUri "https://raw.githubusercontent.com/Cisilion-Ltd/Lighthouse/main/CisilionReader.json"
+        }
+    }
+    
+    if ($result -eq 1){
+        foreach ($sub in $subs) {    
+            Select-AzSubscription -SubscriptionId $sub.Id
+            New-AzDeployment -location uksouth -TemplateUri "https://raw.githubusercontent.com/Cisilion-Ltd/Lighthouse/main/CisilionManager.json"
+        }
     }
 }
     
